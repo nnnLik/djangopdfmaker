4c24346f36c73cb101ed2590710a62f7721ad44c
nnnLik <rslmakhmudov@gmail.com>
2023-11-17 01:47:08 +0300

    `add`: swagger

--- djangopdfmaker/config/urls.py
+++ djangopdfmaker/config/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from src.routes import routes
+
+from .yasg import schema_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include(routes)),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger"),
+        name="schema-swagger-ui",
+    ),
 ]
