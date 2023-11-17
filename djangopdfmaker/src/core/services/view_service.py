6ff0f615391758e6e24864884271ee2f149ac649
nnnLik <rslmakhmudov@gmail.com>
2023-11-17 05:34:53 +0300

    `add`: in memory uploaded file typing

--- djangopdfmaker/src/core/services/view_service.py
+++ djangopdfmaker/src/core/services/view_service.py
@@ -14,18 +14,19 @@ class ViewServices:
         task_id = task_obj.id
 
         to_pdf_validated = (
-            to_pdf if isinstance(to_pdf, str) else task_obj.html_file.path
+            to_pdf if isinstance(to_pdf, str) else task_obj.html_source_file.path
         )
 
         process_to_pdf.delay(task_id, content_type, to_pdf_validated)
 
         return (task_id, "Processing started.")
 
-    def _create_task(self, to_pdf: str, content_type: str) -> Task:
+    def _create_task(
+        self, to_pdf: Union[str, InMemoryUploadedFile], content_type: str
+    ) -> Task:
         return Task.objects.create(
-            url=to_pdf if content_type == "url" else None,
-            html_file=to_pdf if content_type == "file" else None,
-            # user=... # TODO
+            source_url=to_pdf if content_type == "url" else None,
+            html_source_file=to_pdf if content_type == "file" else None,
         )
 
 
